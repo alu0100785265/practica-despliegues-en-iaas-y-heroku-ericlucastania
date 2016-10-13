09a3bbeb2ad6ca7743e950836b68356413a61ea9
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-13 11:36:30 +0000

    primera prueba

---
+++ copiada.js
@@ -0,0 +1,7 @@
+exec("git pull ",puts(error, stdout, stderr) {
+      console.log(stdout)
+      if(err)
+    })
+  },
+  var exec= require('child_process').exec
+  app.use(express.static(__dirname))
