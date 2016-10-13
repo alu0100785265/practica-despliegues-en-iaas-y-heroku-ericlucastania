4610019dd377c11171c39167bae176a50a34a5cc
alu0100786330 <alu0100786330@ull.edu.es>
2016-10-13 13:23:57 +0000

    primera prueba

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post('/synchronize', (request, response) => {
         }
      }
      
-    exec("git pull https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-ericlucastania master",puts);
+    exec("web: node app.js",puts);
 });
 
 
