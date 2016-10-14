d751fc5e47e2c79261aa3d1c692848def5aa0dc3
Eric <alu0100786330@ull.edu.es>
2016-10-14 11:04:17 +0000

    :pig:

--- app.js
+++ app.js
@@ -5,34 +5,13 @@ var express = require('express');
 var app = express();
 var  path = require('path');
 var  expressLayouts = require('express-ejs-layouts');
-var process = require('child_process');
+var proces = require('child_process');
 
 
 
-app.get('/get', function(request, response){
-
-  process.exec("ls", (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(stdout);
-      });
-});
-
-app.post('/syn', function(request, response){
-    process.exec("git pull https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-ericlucastania.git", (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(stdout);
-      });
-  });
 
 
-
-app.set('port',8080);
+app.set('port', (process.env.PORT || 8080));
 
 
 app.use(expressLayouts);
@@ -46,9 +25,26 @@ app.get('/', function(request, response){
 });
 
 
+app.get('/get', function(request, response){
 
+  proces.exec("ls", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+});
 
-
+app.post('/syn', function(request, response){
+    proces.exec("git pull https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-ericlucastania.git", (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    });
+});
 
 
 
